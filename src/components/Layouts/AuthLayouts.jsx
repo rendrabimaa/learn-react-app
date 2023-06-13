05aee68087ec4fde6663462a8fa0e9283752f8ea
rendrabimaa <rendrabimaa@gmail.com>
2023-06-13 16:52:52 +0700

    implementasi-react-router

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -3,12 +3,14 @@ const AuthLayouts = (props) => {
   const {children, title} = props
 
   return (
-    <div className="w-full max-w-xs">
-        <h1 className="text-blue-600 text-3xl font-bold mb-2">
-            {title}
-        </h1>
-        <p className="font-medium text-slate-500">Welcome, Please enter your Details</p>
-        {children}
+    <div className='flex justify-center min-h-screen items-center bg-white'>
+      <div className="w-full max-w-xs">
+          <h1 className="text-blue-600 text-3xl font-bold mb-2">
+              {title}
+          </h1>
+          <p className="font-medium text-slate-500">Welcome, Please enter your Details</p>
+          {children}
+      </div>
     </div>
   )
 }
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,12 @@
 import FormRegister from "../components/Fragments/FormRegister"
 import AuthLayouts from "../components/Layouts/AuthLayouts"
+import { Link } from "react-router-dom"
 
 const RegisterPage = () => {
     return (
-        <AuthLayouts title="Login">
+        <AuthLayouts title="Register">
             <FormRegister />
+            <p className="text-sm mt-5 text-center">Have an account <Link to="/login" className="font-bold text-blue-600">Log In</Link></p>
         </AuthLayouts>
     )
 }
