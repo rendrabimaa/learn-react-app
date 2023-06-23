43f9f1107ccb3e4feef64b806d2aa0ad3c79b0b3
rendrabimaa <rendrabimaa@gmail.com>
2023-06-23 13:52:17 +0700

    implementasi custom hooks

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -2,7 +2,7 @@ import CardProduct from "../components/Fragments/CardProduct"
 import Button from "../components/Elements/Button"
 import { useEffect, useRef, useState } from "react"
 import getProducts from "../services/product.service"
-import { getUsername } from "../services/auth.service"
+import { useLogin } from "../hooks/useLogin"
 
 
 
@@ -10,16 +10,7 @@ const ProductsPage = () => {
     const [cart, setCart] = useState([])
     const [totalPrice, setTotalPrice] = useState(0)
     const [products, setProducts] = useState([])
-    const [username, setUsername] = useState("")
-
-    useEffect(() => {
-        const token = localStorage.getItem("token")
-        if(token) {
-            setUsername(getUsername(token))
-        }else {
-            window.location.href = "/login"
-        }
-    }, [])
+    const username = useLogin()
 
     useEffect(() => {
         setCart(JSON.parse(localStorage.getItem("cart")) || [])
